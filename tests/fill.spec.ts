1ec8121b317c611d10046e52c191bc9403ccaff6
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-02-22 17:31:53 +0800

    add ts-jest

--- tests/fill.spec.ts
+++ tests/fill.spec.ts
@@ -1,20 +1,18 @@
-// import "mocha";
-// import { expect } from "chai";
-// import Fill from "../src/mvp/View/fill/fill";
+import Fill from "../src/mvp/View/Fill/Fill";
 
-// describe("Fill tests", () => {
-//   let fill: Fill = new Fill(document.body);
+describe("Fill tests:", () => {
+  let fill: Fill = new Fill(document.body);
 
-//   it("Create fill element", () => {
-//     expect(fill.getFill()).to.be.a("HTMLDivElement");
-//   });
+  it("create fill element", () => {
+    expect(fill.getFill()).toBe("HTMLDivElement");
+  });
 
-//   it("Render fill", () => {
-//     let width: number = 80;
-//     fill.renderFill(width);
-//     let element: HTMLElement = document.querySelector(
-//       ".slider__fill"
-//     ) as HTMLElement;
-//     expect(element.style.width).to.equal(width + "px");
-//   });
-// });
+  // it("Render fill", () => {
+  //   let width: number = 80;
+  //   fill.renderFill(width);
+  //   let element: HTMLElement = document.querySelector(
+  //     ".slider__fill"
+  //   ) as HTMLElement;
+  //   expect(element.style.width).to.equal(width + "px");
+  // });
+});
