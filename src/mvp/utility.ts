6c78d3e6bc7ff856682c8d0009bedf4062817936
Alexander Axenov <alexanderaxenov@mail.ru>
2020-05-20 10:06:03 +0800

    deleted folders #4

--- src/mvp/utility.ts
+++
@@ -1,9 +0,0 @@
-function createElement(tag: string, props: { [key: string]: string }): HTMLElement {
-  const element: HTMLElement = document.createElement(tag);
-
-  Object.keys(props).forEach(key => element.setAttribute(key, props[key]));
-
-  return element;
-}
-
-export default createElement;
