4f284421763ad2acac0e8927b28a5c65cd745213
Alexander Axcenov <alexanderaxenov@mail.ru>
2020-12-05 08:17:13 +0800

    #16 added display of percentage of code coverage by tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: '(/tests/.*|(\\.|/)(spec))\\.tsx?$',
+  coverageThreshold: {
+    "global": {
+      "branches": 50,
+      "functions": 50,
+      "lines": 50,
+      "statements": 50
+    }
+  }
 }
